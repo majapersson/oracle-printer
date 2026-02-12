62e4964f0bd6bb895055b60e47b896f6c434e77f
Maja Persson <persson.maja@gmail.com>
2026-02-12 18:10:31 +0100

    Add config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  typescript: {
+    /**
+     * Typechecking and building will be done in separate steps in the build
+     * pipeline, so it's preferable to know whether each step passes independent
+     * of the other.
+     */
+    ignoreBuildErrors: true,
+  },
+  productionBrowserSourceMaps: true,
+  images: {
+    unoptimized: true,
+  },
+  poweredByHeader: false,
   reactCompiler: true,
 };
 
