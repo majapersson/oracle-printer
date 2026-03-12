f23154721b8e51921556943a96a389eeac614b08
Maja Persson <persson.maja@gmail.com>
2026-03-12 23:19:33 +0100

    Update missed env var names

--- src/helpers/sendToPrinterAction.ts
+++ src/helpers/sendToPrinterAction.ts
@@ -4,11 +4,13 @@ import { exec } from "child_process";
 import { getOutputPath } from "./getOutputPath";
 
 export async function sendToPrinterAction(filename: string) {
-  const windowsPrinter = process.env.WINDOWS_PRINTER_NAME;
-  const windowsComputerName = process.env.WINDOWS_COMPUTER_NAME;
+  const windowsPrinter = process.env.NEXT_PUBLIC_WINDOWS_PRINTER_NAME;
+  const windowsComputerName = process.env.NEXT_PUBLIC_WINDOWS_COMPUTER_NAME;
 
   if (windowsPrinter && windowsComputerName && process.platform === "win32") {
     const outputPath = getOutputPath(filename);
-    exec(`print \/D:\\\\${windowsComputerName}\\${windowsPrinter} ${outputPath}`);
+    exec(
+      `print \/D:\\\\${windowsComputerName}\\${windowsPrinter} ${outputPath}`,
+    );
   }
 }
